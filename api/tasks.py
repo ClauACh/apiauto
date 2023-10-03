0c9683e2cc431ab463dd5d0ed6e0cf8470f0cd75
Claudia Aparicio Chavez <Claudia.Aparicio@jalasoft.com>
2023-10-02 23:35:53 -0400

    Tasks fixes and validations added

--- api/tasks.py
+++ api/tasks.py
@@ -10,6 +10,7 @@ import unittest
 import requests
 
 from api.todo_base import TodoBase
+from api.validate_response import ValidateResponse
 from config.config import HEADERS
 from utils.logger import get_logger
 from utils.rest_client import RestClient
@@ -18,74 +19,119 @@ LOGGER = get_logger(__name__, logging.DEBUG)
 
 
 class Tasks(unittest.TestCase):
-
+    """
+    Class for tasks endpoint
+    """
     @classmethod
     def setUpClass(cls):
         cls.url_tasks = "https://api.todoist.com/rest/v2/tasks"
         cls.session = requests.Session()
 
-        cls.project_id = TodoBase().get_all_projects().json()[1]["id"]
-        cls.section_id = TodoBase().get_all_sections().json()[1]["id"]
-        cls.task_id = TodoBase().get_all_tasks().json()[1]["id"]
-    def test_create_task(self):
+        cls.project_id = TodoBase().get_all_projects()["body"][1]["id"]
+        cls.section_id = TodoBase().get_all_sections()["body"][1]["id"]
+        cls.task_id = TodoBase().get_all_tasks()["body"][1]["id"]
 
+    def test_create_task(self):
+        """
+        Test create a task
+        :return:
+        """
         response = self.create_task()
-        assert response.status_code == 200
+        ValidateResponse().validate_response(actual_response=response, method="post",
+                                             expected_status_code=200,
+                                             feature="task")
 
     def test_create_task_with_project_id(self):
+        """
+        Test to create a task in a project
+        :return:
+        """
         project_id = self.project_id
         response = self.create_task(project_id=project_id)
-        assert response.status_code == 200
+        ValidateResponse().validate_response(actual_response=response, method="post",
+                                             expected_status_code=200,
+                                             feature="task")
 
     def test_create_task_with_section_id(self):
+        """
+        Test to create a task in a section
+        :return:
+        """
         section_id = self.section_id
         response = self.create_task(section_id=section_id)
-        assert response.status_code == 200
+        ValidateResponse().validate_response(actual_response=response, method="post",
+                                             expected_status_code=200,
+                                             feature="task")
 
     def test_get_all_tasks(self):
-
+        """
+        Test to get all the tasks
+        :return:
+        """
         response = TodoBase().get_all_tasks()
-        LOGGER.info("Number of tasks returned: %s", len(response.json()))
-        assert response.status_code == 200
+        LOGGER.info("Number of tasks returned: %s", len(response["body"]))
+        ValidateResponse().validate_response(actual_response=response, method="get",
+                                             expected_status_code=200,
+                                             feature="tasks")
 
     def test_get_task_by_id(self):
+        """
+        Test to get a task by id
+        :return:
+        """
         task_id = self.task_id
         LOGGER.info("Task Id: %s", task_id)
         url_task = f"{self.url_tasks}/{task_id}"
         response = RestClient().send_request("get", session=self.session, headers=HEADERS,
                                              url=url_task)
 
-        assert response.status_code == 200
+        ValidateResponse().validate_response(actual_response=response, method="get",
+                                             expected_status_code=200,
+                                             feature="task")
 
     def test_close_task(self):
-
+        """
+        Test to close a task
+        :return:
+        """
         task_id = self.task_id
         LOGGER.info("Task Id: %s", task_id)
         url_task_close = f"{self.url_tasks}/{task_id}/close"
         response = RestClient().send_request("post", session=self.session, headers=HEADERS,
                                              url=url_task_close)
 
-        assert response.status_code == 204
+        ValidateResponse().validate_response(actual_response=response, method="close",
+                                             expected_status_code=204,
+                                             feature="task")
 
     def test_reopen_task(self):
+        """
+        Test of reopening a task
+        :return
+        """
         # valid task open
-        task_id = self.create_task().json()["id"]
+        task_id = self.create_task()["body"]["id"]
 
         # close
         url_task_close = f"{self.url_tasks}/{task_id}/close"
         response_close = RestClient().send_request("post", session=self.session, headers=HEADERS,
                                                    url=url_task_close)
 
-        assert response_close.status_code == 204
+        # assert response_close.status_code == 204
 
         LOGGER.info("Task Id: %s", task_id)
         url_task_reopen = f"{self.url_tasks}/{task_id}/reopen"
         response = RestClient().send_request("post", session=self.session, headers=HEADERS,
                                              url=url_task_reopen)
 
-        assert response.status_code == 204
+        ValidateResponse().validate_response(actual_response=response, method="reopen",
+                                             expected_status_code=204,
+                                             feature="task")
 
     def create_task(self, project_id=None, section_id=None):
+        """
+            Test for creating a task
+        """
         data = {
             "content": "Task inside section",
             "due_string": "tomorrow at 12:00",
@@ -120,12 +166,23 @@ class Tasks(unittest.TestCase):
         response = RestClient().send_request("post", session=self.session, headers=HEADERS,
                                              url=url_task, data=data)
 
-        assert response.status_code == 200
+        ValidateResponse().validate_response(actual_response=response, method="post",
+                                             expected_status_code=200, feature="task")
+
     def test_delete_task(self):
-            """
-            Test to delete session
-            :return:
-            """
-            response = RestClient().send_request("delete", session=self.session, url=self.url_task,
+        """
+        Test to delete session
+        :return:
+        """
+        task_id = self.task_id
+        LOGGER.info("Task Id: %s", task_id)
+        url_task = f"{self.url_tasks}/{task_id}"
+        response = RestClient().send_request("delete", session=self.session, url=url_task,
                                                  headers=HEADERS)
-            assert response.status_code == 204
+        ValidateResponse().validate_response(actual_response=response, method="delete",
+                                             expected_status_code=204,
+                                             feature="task")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDown Class")
